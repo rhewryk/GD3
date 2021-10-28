4f06013789dd3047f5667895ecfbc2dea2d68396
rhewryk <78235971+rhewryk@users.noreply.github.com>
2021-10-28 13:06:42 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,75 @@
+console.log("this file is linked");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.onload = function(){
+
+// 	function changeColor(){
+// 		$("p").css("color", "green");
+// 		$("p").css("font-size", "100px");
+// 	}
+
+// 	function changeBack(){
+// 		$("p").css("color", "blue");
+// 		$("p").css("font-size", "10px");
+// 	}
+// 		$(window).keypress(changeColor);
+// 		// $("p").hover(changeColor, changeBack);
+// 		// $(window).scroll(changeColor);
+
+// 		var city = "toronto";
+
+// 		if (city == "toronto"){
+// 			alert("You are in Toronto");
+// 		} else {
+// 			alert("You are not in Toronto");
+// 		}
+// }
+
+
+
+
+
+
+// function add(num1, num2){
+// 	var result = num1 + num2;
+// 	return result;
+// }
+
+// var sum = add(1, 5);
+// var newNum = add(5000,20);
+
+// console.log(sum);
+// console.log(newNum);
+
+
+
+
+// var city = "toronto";
+// console.log(city);
+
+// var year = 2021;
+// console.log(year);
+
+// var classStarted = true;
+// console.log(classStarted);
+
+// // var math = 548 * 389;
+// // console.log(math);
+
+// var yearTwo = 20;
+
+// var newYear = city + year;
+// console.log(newYear);
